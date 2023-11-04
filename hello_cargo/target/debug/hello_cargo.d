2d32d2e0c3562b4be0108ff2b1632fd2e4141378
claddyk <madscientists1523@gmail.com>
2023-11-04 19:49:41 +0530

    delete hello world: very noob

--- hello_cargo/target/debug/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/claddy/Documents/wr/rustismusst/hello_cargo/target/debug/hello_cargo: /home/claddy/Documents/wr/rustismusst/hello_cargo/src/main.rs
--- hello_cargo/target/release/hello_cargo.d
+++
@@ -1,1 +0,0 @@
-/home/claddy/Documents/wr/rustismusst/hello_cargo/target/release/hello_cargo: /home/claddy/Documents/wr/rustismusst/hello_cargo/src/main.rs
