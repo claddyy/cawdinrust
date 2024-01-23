25ff437ac714f5c3d029272b38c052cf63aa1bb3
claddyy <madscientists1523@gmail.com>
2024-01-24 02:00:17 +0530

    methods n more structs

--- nubconcepts/src/main.rs
+++ nubconcepts/src/main.rs
@@ -1,41 +1,22 @@
-fn structex() {
-    let user1 = User {
-        email: String::from("madscientists1523@gmail.com"),
-        username: String::from("claddy"),
-        active: true,
-        sign_in_count: 1,
-    };
-
-    let user2 = User {
-        email: String::from("mkgeek91@gmail.com"),
-        ..user1
-    };
-}
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
-}
-
 #[derive(Debug)]
-
 struct Rectangle {
     width: u32,
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
+    let rect2 = Rectangle { width: 10, height: 40 };
+    let rect3 = Rectangle { width: 60, height: 45 };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        area(&rect1)
-    );
-    println!("rect1 is {:#?}", rect1);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
